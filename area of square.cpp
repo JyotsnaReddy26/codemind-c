ae7d070a84caac4c427defac46157203c0b368fe
JyotsnaReddy26 <147791987+JyotsnaReddy26@users.noreply.github.com>
2024-03-01 21:45:02 +0530

    Add files via upload

---
+++ area of square.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int side,area;
+	printf("enter side");
+	scanf("%d",&side);
+	area=side*side;
+	printf("Area of square is %d",area);
+	printf("square having all side same");
+	
+
+}
+
